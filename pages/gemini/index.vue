56540b319508f1455af3bd5207e0b2b208131e0c
AlvarDev <alvardev.lp@gmail.com>
2024-07-14 21:25:55 -0300

    adding gemini page

--- pages/gemini/index.vue
+++ pages/gemini/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <!-- You can add content here if you want, but it will be replaced by the redirect -->
+    <v-btn to="/gemini" color="primary">Try Gemini</v-btn>
   </div>
 </template>
 
@@ -8,7 +9,7 @@
 export default {
   mounted() {
     // Redirect to the new site
-    window.location.href = 'https://www.google.com/'; // Replace with your desired URL
+    // window.location.href = 'https://www.google.com/'; // Replace with your desired URL
   }
 };
-</script>
+</script>   
