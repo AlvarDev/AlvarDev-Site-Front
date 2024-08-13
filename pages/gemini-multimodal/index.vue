cf1bf46fae537e704e75015b20515c47dd47c597
AlvarDev <alvardev.lp@gmail.com>
2024-08-13 17:01:30 -0300

    Adding Gemini Multimodal Slide

---
+++ pages/gemini-multimodal/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQk89ELTJDMNwwoiTIYvbcs7Fn5Y80XOQwFEorAej8oC_ySXCHu6srBEAufDUbQKYGXnPTDI8qizkqI/embed?start=false&loop=false&delayms=3000" frameborder="0" style="width: 100%; height: 100%;" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+</template>
+  
+  <!-- <script>
+  export default {
+    mounted() {
+      // Redirect to the new site
+      window.location.href = 'https://docs.google.com/presentation/d/e/2PACX-1vQk89ELTJDMNwwoiTIYvbcs7Fn5Y80XOQwFEorAej8oC_ySXCHu6srBEAufDUbQKYGXnPTDI8qizkqI/pub?start=false&loop=false&delayms=3000'; // Replace with your desired URL
+    }
+  };
+  </script> -->
