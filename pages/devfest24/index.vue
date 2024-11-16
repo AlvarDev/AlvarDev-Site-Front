3d67bad17993e925b4bb976d82816124fd6d9373
AlvarDev <alvardev.lp@gmail.com>
2024-11-16 09:31:47 -0300

    DevFest24

---
+++ pages/devfest24/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <iframe src="https://docs.google.com/presentation/d/1_c_6YxoXOwMPoXJYvfa4IvxSeLyAHjSJ0u6zdKuHUU4/edit?usp=sharing" frameborder="0" style="width: 100%; height: 100%;" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+</template>
