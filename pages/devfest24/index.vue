e705c075a34a04e593299a05f36a3a18d634c7d3
AlvarDev <alvardev.lp@gmail.com>
2024-11-16 09:42:59 -0300

    redirect devfest

--- pages/devfest24/index.vue
+++ pages/devfest24/index.vue
@@ -1,3 +1,14 @@
 <template>
-    <iframe src="https://docs.google.com/presentation/d/1_c_6YxoXOwMPoXJYvfa4IvxSeLyAHjSJ0u6zdKuHUU4/edit?usp=sharing" frameborder="0" style="width: 100%; height: 100%;" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
-</template>
+    <div>
+      <!-- You can add content here if you want, but it will be replaced by the redirect -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    mounted() {
+      // Redirect to the new site
+      window.location.href = 'https://docs.google.com/presentation/d/1_c_6YxoXOwMPoXJYvfa4IvxSeLyAHjSJ0u6zdKuHUU4/edit?usp=sharing'; // Replace with your desired URL
+    }
+  };
+  </script>
